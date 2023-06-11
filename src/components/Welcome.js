8fd57e739cfc5d44d214a563bcccbbfaec909bad
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-11 22:32:37 +0530

    submitting code-Sun Jun 11 2023 22:32:36 GMT+0530 (India Standard Time)

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,11 @@
 // write code for Welcome component here
+function Welcome(props){
+    return(
+        <>
+            <h1>Hey {props[0]}!</h1>
+            <h2>Welcome to school.</h2>
+        </>
+    )
+}
+
+export default Welcome;
